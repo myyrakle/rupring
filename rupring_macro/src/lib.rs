de3418f616daf7006fe832fce92f8577fb4580b9
myyrakle <sssang97@naver.com>
2023-10-08 23:18:41 +0900

    chore: add Get/Post/Put/Delete/Patch macro

--- rupring_macro/src/lib.rs
+++ rupring_macro/src/lib.rs
@@ -20,3 +20,38 @@ pub fn Injectable(attr: TokenStream, item: TokenStream) -> TokenStream {
     // ...
     return item;
 }
+
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn Get(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // ...
+    return item;
+}
+
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn Post(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // ...
+    return item;
+}
+
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn Put(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // ...
+    return item;
+}
+
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn Delete(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // ...
+    return item;
+}
+
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn Patch(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // ...
+    return item;
+}
