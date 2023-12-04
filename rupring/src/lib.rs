c99c0ab135a1b284711a581a9ebe0419d75c725d
myyrakle <sssang97@naver.com>
2023-12-05 00:13:18 +0900

    [#3] 약간의 인터페이스 추가

--- rupring/src/lib.rs
+++ rupring/src/lib.rs
@@ -1,6 +1,23 @@
 pub use rupring_macro::{Controller, Delete, Get, Injectable, Module, Patch, Post, Put};
 
-pub trait IModule {}
+pub trait IModule {
+    fn child_modules(&self) -> Vec<Box<dyn IModule>>;
+    fn controllers(&self) -> Vec<Box<dyn IController>>;
+}
+
+pub trait IController {
+    fn routes(&self) -> Vec<Box<dyn IRoute>>;
+}
+
+pub trait IRoute {
+    fn method(&self) -> String;
+    fn path(&self) -> String;
+    fn handler(&self) -> Box<dyn IHandler>;
+}
+
+pub trait IHandler {
+    fn handle(&self);
+}
 
 pub struct Rupring<T: IModule> {
     root_module: T,
