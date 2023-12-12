d7d7595faf90b664d874c275c7d6a87e8143ee86
myyrakle <sssang97@naver.com>
2023-12-13 04:41:13 +0900

    [#34] docs: summary descriptions

--- rupring/src/lib.rs
+++ rupring/src/lib.rs
@@ -2,19 +2,77 @@ pub(crate) mod boot;
 pub(crate) mod request;
 pub(crate) mod response;
 
-pub use rupring_macro::{Controller, Delete, Get, Module, Patch, Post, Put};
+/// Controller Annotation
+/// ```
+/// #[derive(Debug, Clone)]
+/// #[rupring::Controller(prefix=/, routes=[hello, echo])]
+/// pub struct HomeController {}
+/// ```
+pub use rupring_macro::Controller;
+
+/// Module Annotation
+/// ```
+/// #[derive(Debug, Clone, Copy)]
+/// #[rupring::Module(
+///    controllers=[HomeController{}],
+///    modules=[],
+///    providers=[HomeService::default(), HomeRepository::default(), UserService::default(), CounterService::default()]
+/// )]
+/// pub struct RootModule {}
+pub use rupring_macro::Module;
+
+/// Get Route Annotation
+/// ```
+/// #[rupring::Get(path = /)]
+/// pub fn hello(request: rupring::Request) -> rupring::Response {
+///    // ...
+/// }
+pub use rupring_macro::Get;
+
+/// Post Route Annotation
+/// ```
+/// #[rupring::Post(path = /)]
+/// pub fn hello(request: rupring::Request) -> rupring::Response {
+///   // ...
+/// }
+pub use rupring_macro::Post;
+
+/// Patch Route Annotation
+/// ```
+/// #[rupring::Patch(path = /)]
+/// pub fn hello(request: rupring::Request) -> rupring::Response {
+///   // ...
+/// }
+pub use rupring_macro::Patch;
+
+/// Put Route Annotation
+/// ```
+/// #[rupring::Put(path = /)]
+/// pub fn hello(request: rupring::Request) -> rupring::Response {
+///   // ...
+/// }
+pub use rupring_macro::Put;
+
+/// Delete Route Annotation
+/// ```
+/// #[rupring::Delete(path = /)]
+/// pub fn hello(request: rupring::Request) -> rupring::Response {
+///     // ...
+/// }
+pub use rupring_macro::Delete;
 
 extern crate rupring_macro;
 
+/// HTTP method (from hyper crate)
 pub type Method = hyper::Method;
 
 /// Dependency Injection Context for entire life cycle
 pub use boot::di::DIContext;
 /// Dependency Injection Provider
 pub use boot::di::IProvider;
-/// Request
+/// HTTP Request
 pub use request::Request;
-/// Response
+/// HTTP Response
 pub use response::Response;
 
 /// Module interface
@@ -42,18 +100,21 @@ pub trait IHandler {
     fn handle(&self, request: Request) -> Response;
 }
 
+/// Rupring Factory for creating server
 #[derive(Debug, Clone)]
 pub struct RupringFactory<T: IModule> {
     root_module: T,
 }
 
 impl<T: IModule + Clone + Copy + Sync + Send + 'static> RupringFactory<T> {
+    /// It receives the root module object and creates a factory to run the server.
     pub fn create(module: T) -> Self {
         RupringFactory {
             root_module: module,
         }
     }
 
+    /// It receives the port number and runs the server.
     pub async fn listen(self, port: u16) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
