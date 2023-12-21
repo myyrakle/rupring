ccfff0d64edf749b12b78950502c3718bb6dd442
myyrakle <sssang97@naver.com>
2023-12-22 01:54:55 +0900

    [#31] add: app info logging

--- rupring/src/boot/banner.rs
+++ rupring/src/boot/banner.rs
@@ -7,4 +7,12 @@ const RUPRING_TEXT: &'static str = r#",------.                       ,--.
 
 pub fn print_banner() {
     println!("{}", RUPRING_TEXT);
+    print_app_info();
+    println!("");
+}
+
+fn print_app_info() {
+    let version = env!("CARGO_PKG_VERSION");
+    let name = env!("CARGO_PKG_NAME");
+    println!("{name} v{version}");
 }
