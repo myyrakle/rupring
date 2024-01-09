a0f8477d0106091e20f68b7208e733656ae34789
myyrakle <sssang97@naver.com>
2024-01-10 03:08:46 +0900

    [#52] add: image/* meme type 상수 정의

--- rupring/src/meme.rs
+++ rupring/src/meme.rs
@@ -3,3 +3,16 @@ pub const TEXT: &str = "text/plain";
 pub const HTML: &str = "text/html";
 pub const CSS: &str = "text/css";
 pub const JAVASCRIPT: &str = "text/javascript";
+
+// image 계열 MEME 타입
+pub const PNG: &str = "image/png";
+pub const JPEG: &str = "image/jpeg";
+pub const GIF: &str = "image/gif";
+pub const SVG: &str = "image/svg+xml";
+pub const ICO: &str = "image/x-icon";
+pub const WEBP: &str = "image/webp";
+pub const BMP: &str = "image/bmp";
+pub const TIF: &str = "image/tiff";
+pub const PSD: &str = "image/vnd.adobe.photoshop";
+pub const DWG: &str = "image/vnd.dwg";
+pub const DXF: &str = "image/vnd.dxf";
