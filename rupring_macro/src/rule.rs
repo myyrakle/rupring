3603bc1e287010e3eb7517d8022853534e20c557
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2024-09-20 09:40:04 +0900

    fix: change name convention to camel case

--- rupring_macro/src/rule.rs
+++ rupring_macro/src/rule.rs
@@ -1,7 +1,7 @@
 pub(crate) fn make_route_name(function_name: &str) -> String {
     let mut route_name = String::new();
 
-    route_name.push_str(&format!("Route_{}", function_name));
+    route_name.push_str(&format!("Route{}", function_name.chars().next().unwrap().to_uppercase().chain(function_name.chars().skip(1)))));
 
     return route_name;
 }
@@ -9,7 +9,7 @@ pub(crate) fn make_route_name(function_name: &str) -> String {
 pub(crate) fn make_handler_name(function_name: &str) -> String {
     let mut handler_name = String::new();
 
-    handler_name.push_str(&format!("Handler_{}", function_name));
+    handler_name.push_str(&format!("Handler{}", function_name.chars().next().unwrap().to_uppercase().chain(function_name.chars().skip(1)))));
 
     return handler_name;
 }
