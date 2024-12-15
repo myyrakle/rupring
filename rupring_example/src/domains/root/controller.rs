8a90c11bdaf80484354cc08a776a1f0dafa5fd94
myyrakle <sssang97@naver.com>
2024-12-15 22:41:14 +0900

    [#174] example: slow api 추가

--- rupring_example/src/domains/root/controller.rs
+++ rupring_example/src/domains/root/controller.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, Clone)]
-#[rupring::Controller(prefix=/, routes=[index], tags=["root"])]
+#[rupring::Controller(prefix=/, routes=[index, slow], tags=["root"])]
 pub struct RootController {}
 
 #[rupring::Get(path = /)]
@@ -9,6 +9,19 @@ pub struct RootController {}
 pub fn index(request: rupring::Request) -> rupring::Response {
     let body = request.body;
     println!("body: {}", body);
+
+    rupring::Response::new().text("123214")
+}
+
+#[rupring::Get(path = /slow)]
+#[summary = "그냥 느린 API입니다."]
+#[description = "별다른 기능은 없습니다."]
+#[tags = [root]]
+pub fn slow(request: rupring::Request) -> rupring::Response {
+    let body = request.body;
+    println!("body: {}", body);
+
+    std::thread::sleep(std::time::Duration::from_secs(30));
 
     rupring::Response::new().text("123214")
 }
